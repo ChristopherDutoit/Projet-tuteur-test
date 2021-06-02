24e248edca0fb65c245e85b51e9c9651209a6f29
christopher <christopher.dutoit2002@gmail.com>
2021-06-02 13:46:38 +0200

    amélioration  diapo

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -2,24 +2,37 @@ var images=[];
 images[0]="images/ConventionFond.jpg";
 images[1]="images/ConventionFond2.jpg";
 images[2]="images/ConventionFond3.jpg";
-images[3]="images/ConventionFond4";
- var i=0;
+ var i= -1*1;
  var timer=5000;
 var suiv;
 
  function imagesuiv(stop){
    console.log(i);
-   document.getElementById('imagediapo').src = images[i];
+
+if(i==-1){
+  timer = 15000;
+  document.getElementById('imagediapo').style.display = "none";
+  document.getElementById('pub').style.display = "block";
+  document.getElementById('pub').src="https://www.youtube.com/embed/Olj-4K5qf00?rel=0&autoplay=1";
+  stop = "false";
+}
     if(i<images.length-1){
       i++;
+      timer = 5000;
+      document.getElementById('pub').src="";
+        document.getElementById('pub').style.display = "none";
+      document.getElementById('imagediapo').src = images[i];
+      document.getElementById('imagediapo').style.display = "block";
+
     }
     else{
-      i=0;
+      i= -1*1;
+
     }
 
 console.log(stop)
   if (stop == false){
-  suiv = setTimeout("imagesuiv(false)", timer);
+   setTimeout("imagesuiv(false)", timer);
   }
  }
 
