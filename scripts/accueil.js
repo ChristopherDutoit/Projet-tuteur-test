6533ae47270955250696b22a66dd857f0c057f70
christopher <christopher.dutoit2002@gmail.com>
2021-06-02 16:32:19 +0200

    amélioration  diapo

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -2,39 +2,45 @@ var images=[];
 images[0]="images/ConventionFond.jpg";
 images[1]="images/ConventionFond2.jpg";
 images[2]="images/ConventionFond3.jpg";
- var i= -1*1;
+ var i= -1;
  var timer=5000;
 var suiv;
 
  function imagesuiv(stop){
-   console.log(i);
 
-if(i==-1){
-  timer = 15000;
+
+if(i== -1){
+   console.log(i);
+  timer = 35000;
   document.getElementById('imagediapo').style.display = "none";
   document.getElementById('pub').style.display = "block";
-  document.getElementById('pub').src="https://www.youtube.com/embed/Olj-4K5qf00?rel=0&autoplay=1";
-  stop = "false";
-}
-    if(i<images.length-1){
-      i++;
+  document.getElementById('pub').src="https://www.youtube.com/embed/9R1K0BzetwA?rel=0&autoplay=1";
+
+
+} else{
       timer = 5000;
       document.getElementById('pub').src="";
         document.getElementById('pub').style.display = "none";
+         console.log(i);
       document.getElementById('imagediapo').src = images[i];
       document.getElementById('imagediapo').style.display = "block";
 
+
     }
+if(i<images.length-1){
+    i++;}
     else{
-      i= -1*1;
-
+      i=-1;
     }
 
-console.log(stop)
   if (stop == false){
+    console.log(stop)
+    console.log(timer);
    setTimeout("imagesuiv(false)", timer);
-  }
  }
+
+}
+
 
  function imageprec(){
    if(i>0){
