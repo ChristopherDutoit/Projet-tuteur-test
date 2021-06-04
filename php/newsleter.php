72fd4683f950ad05d13d29a781367e74cd0f05c0
Félix Viart <viart.felix2017@gmail.com>
2021-06-04 15:44:28 +0200

    MàJ (qui marche à moitiée...) de newletter.

--- php/newsleter.php
+++ php/newsleter.php
@@ -9,14 +9,14 @@ require '../PHPMailer/PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer();
 $mail->IsSMTP();
-$mail->Host = 'ssl://smtp.gmail.com';             //Adresse IP ou DNS du serveur SMTP
-$mail->Port = 587;                          //Port TCP du serveur SMTP
+$mail->Host = 'smtp.gmail.com';             //Adresse IP ou DNS du serveur SMTP
+$mail->Port = 465;                          //Port TCP du serveur SMTP
 $mail->SMTPAuth = 1;                        //Utiliser l'identification
 
 if($mail->SMTPAuth){
    $mail->SMTPSecure = 'ssl';               //Protocole de sécurisation des échanges avec le SMTP
    $mail->Username   =  'viart.felix2017@gmail.com';   //Adresse email à utiliser
-   $mail->Password   =  '';                //Mot de passe de l'adresse email à utiliser
+   $mail->Password   =  'fefe2002$*';                //Mot de passe de l'adresse email à utiliser
 }
 
 
@@ -28,12 +28,15 @@ $mail->FromName   = 'Félix';             //L'alias à afficher pour l'envoi
 $mail->Subject    =  'Le sujet';                      //Le sujet du mail
 $mail->WordWrap   = 50; 			                         //Nombre de caracteres pour le retour a la ligne automatique
 $mail->AltBody = 'Si ça marche ptn de merde je hurle'; 	       //Texte brut
-$mail->IsHTML(false);
+$mail->IsHTML(true);
 $mail->Body="Si ça marche POGCHAMP";
-
 $mail->AddAddress('viart.felix2017@gmail.com','Félix Viart');
 
-if (!$mail->send()) {
+$mail->ErrorInfo;
+
+$mail->send();
+
+if ($mail->send()) {
       echo $mail->ErrorInfo;
 } else{
       echo 'Message bien envoyé';
